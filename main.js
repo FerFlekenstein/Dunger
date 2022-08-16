9459616d951acb2baa3f09a813677e7976d3c46b
Fernando Flekenstein <fer.flekenstein96@gmail.com>
2022-08-16 10:26:56 -0300

    agregando API

--- main.js
+++ main.js
@@ -14,13 +14,13 @@ class Objeto {
         this.descripcion = descripcion
     }
 }
+// creacion de personajes y su array
+const lista = JSON.parse(localStorage.getItem("personajes"))?? []
+const formulario = document.getElementById('formulario')
 function crearPersonaje(nombre, clase, raza, vida, imagen){
     const nuevoPj = new Personaje(nombre, clase, raza, vida, imagen)
     lista.push(nuevoPj)
 }
-// creacion de personajes y su array
-const lista = JSON.parse(localStorage.getItem("personajes"))?? []
-const formulario = document.getElementById('formulario')
 formulario.addEventListener('submit', (event) => {
     event.preventDefault()
     let nombre = document.getElementById("nombrePJ").value
@@ -28,50 +28,41 @@ formulario.addEventListener('submit', (event) => {
     let raza = document.getElementById("razaPj").value
     let vida = document.getElementById("vidaPj").value
     switch(clase){
-        case "mago":
+        case "wizard":
             imagen = "./images/mago.jpg"
             break
-        case "guerrero":
+        case "fighter":
             imagen = "./images/guerrero.jpg"
             break
-        case "arquero":
+        case "ranger":
             imagen = "./images/arquero.jpg"
             break
-        case "artificiero":
-            imagen = "./images/artificiero.jpg"
-            break
-        case "barbaro":
+        case "barbarian":
             imagen = "./images/barbaro.jpg"
             break
-        case "bardo":
+        case "bard":
             imagen = "./images/bardo.jpg"
             break
-        case "brujo":
+        case "warlock":
             imagen = "./images/brujo.jpg"
             break
-        case "cazadorDeSangre":
-            imagen = "./images/cazadorDeSangre.jpg"
-            break
-        case "druida":
+        case "druid":
             imagen = "./images/druida.jpg"
             break
-        case "clerigo":
+        case "cleric":
             imagen = "./images/clerigo.jpg"
             break
-        case "hechicero":
+        case "sorcerer":
             imagen = "./images/hechicero.jpg"
             break
-        case "monje":
+        case "monk":
             imagen = "./images/monje.jpg"
             break
         case "paladin":
             imagen = "./images/paladin.jpg"
             break
-        case "picaro":
-            imagen = "./images/picaro.jpg"
-            break
         default:
-            imagen = "./images/signoDePregunta.webp"
+            imagen = "./images/picaro.jpg"
             break
     }
     if(nombre == ""){
@@ -88,12 +79,9 @@ formulario.addEventListener('submit', (event) => {
         formulario.reset()
     }
  })
-// boton mostrar personajes creados
-const mostrarPjs = document.getElementById("mostrarPjs")
-const contenedorPj = document.getElementById("multiCollapseExample0")
-mostrarPjs.addEventListener("click", () => {
-    contenedorPj.innerHTML = ``
-    lista.forEach((personaje, indice) => {
+//Escribe las tarjetas de cada personaje en el html
+function escTarjeta(arr){
+    arr.forEach((personaje, indice) => {
         contenedorPj.innerHTML +=`
         <div class="card text-dark m-3" style="width: 18rem;" id="carta${indice}">
             <div class="row justify-content-end">
@@ -112,31 +100,36 @@ mostrarPjs.addEventListener("click", () => {
                     <h5 class="card-title col-8 ">${personaje.nombre}</h5>
                 </div>
                 <p class="card-text">${personaje.raza}</p>
-
+                <div class="dropdown">
+                    <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">Competencias</button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1" id="comp${indice}">
+                    </ul>
+                </div>
                 <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapseExample1${indice}" aria-expanded="false" aria-controls="multiCollapseExample1${indice}" id="form${indice}">+</button>
-
                 <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapseExample2${indice}" aria-expanded="false" aria-controls="multiCollapseExample2${indice}" id="inventario${indice}">Ver inventario</button>
-
                 <div class="collapse multi-collapse row" id="multiCollapseExample1${indice}">
                 </div>
-
                 <div class="collapse multi-collapse row" id="multiCollapseExample2${indice}">
                 </div>
             </div>
          </div>
         `
     })
-    // boton eliminar personajes
-    lista.forEach((personaje, indice) => {
+}
+//Boton para eliminar pj. Toma como parametro un array
+ function eliminarPj(arr){
+    arr.forEach((personaje, indice) => {
         let botonEliminar = document.getElementById(`carta${indice}`).firstElementChild.lastElementChild
         botonEliminar.addEventListener('click', () => {
             document.getElementById(`carta${indice}`).remove()
-            lista.splice(indice, 1)
+            arr.splice(arr.indexOf(personaje), 1)
             localStorage.setItem('personajes', JSON.stringify(lista))
         })
     })
-    // boton mostrar form objetos
-    lista.forEach((personaje, indice) => {
+}
+//Crea boton para mostrar formulario de objetos, y crea los objetos
+function almcenarObj(arr){
+    arr.forEach((personaje, indice) => {
         let botonFormObjeto = document.getElementById(`form${indice}`)
         let contFormObjeto = document.getElementById(`multiCollapseExample1${indice}`)
         botonFormObjeto.addEventListener("click", () =>{
@@ -167,9 +160,11 @@ mostrarPjs.addEventListener("click", () => {
             })
         })
     })
-    // boton mostrar inventario de cada personaje 
-    lista.forEach((personaje, indice) => {
-        const inventarioStorage = JSON.parse(localStorage.getItem(`objetos${indice}`)) ?? []
+}
+//Crea boton para mostrar inventario y el boton para eliminar cada objeto
+function verInventario(arr){
+    arr.forEach((personaje, indice) => {
+        const inventarioStorage = JSON.parse(localStorage.getItem(`objetos${indice}`)) ?? personaje.inventario
         let mostrarInventario = document.getElementById(`inventario${indice}`)
         let contenedorInventario = document.getElementById(`multiCollapseExample2${indice}`)
         mostrarInventario.addEventListener("click", () =>{
@@ -187,26 +182,53 @@ mostrarPjs.addEventListener("click", () => {
             })
             // boton eliminar objetos de inventario
             inventarioStorage.forEach((objeto, indice) => {
-            let botonObjeto = document.getElementById(`item${indice}`).firstElementChild.lastElementChild
-            botonObjeto.addEventListener('click', () => {
-                document.getElementById(`item${indice}`).remove()
-                personaje.inventario.splice(indice, 1)
-                localStorage.setItem(`objetos${indice}`, JSON.stringify(personaje.inventario))
+                let botonObjeto = document.getElementById(`item${indice}`).firstElementChild.lastElementChild
+                botonObjeto.addEventListener('click', () => {
+                    document.getElementById(`item${indice}`).remove()
+                    personaje.inventario.splice(indice, 1)
+                    localStorage.setItem(`objetos${indice}`, JSON.stringify(personaje.inventario))
                 })
             })
         })
     })
+}
+//Consulta la api de D&D y escribe los datos en el html. No necesita parametros
+async function apiClase(){
+    lista.forEach((personaje, indice) => {
+        let competencias = document.getElementById(`comp${indice}`)
+        fetch(`https://www.dnd5eapi.co/api/classes/${personaje.clase}`)
+        .then(respuesta => respuesta.json())
+        .then(info => {
+            let {proficiencies} = info
+            competencias.innerHTML = ""
+            proficiencies.forEach((element, indice) => {
+                let {name} = element
+                competencias.innerHTML += `
+                <li><a class="dropdown-item">${indice + 1}: ${name}</a></li>
+                `
+            })
+        })
+    })
+}
+// boton mostrar personajes creados
+const mostrarPjs = document.getElementById("mostrarPjs")
+const contenedorPj = document.getElementById("multiCollapseExample0")
+mostrarPjs.addEventListener("click", () => {
+    contenedorPj.innerHTML = ``
+    escTarjeta(lista)
+    almcenarObj(lista)
+    verInventario(lista)
+    eliminarPj(lista)
+    apiClase()
 })
 // Modo noche/dia
 const modoClaro = document.getElementById("modoClaro")
 const modoOscuro = document.getElementById("modoOscuro")
 let oscuro
 (localStorage.getItem("tema"))? oscuro = localStorage.getItem("tema") : localStorage.setItem("tema", "claro")
-
 if(oscuro == "oscuro"){
     document.body.classList.add("modoOscuro")
 }
-
 modoOscuro.addEventListener("click", () =>{
     document.body.classList.add("modoOscuro")
     localStorage.setItem("tema", "oscuro")
@@ -215,7 +237,7 @@ modoClaro.addEventListener("click", () =>{
     document.body.classList.remove("modoOscuro")
     localStorage.setItem("tema", "claro")
 })
-// dado
+// botones para girar dados de diferentes caras segun el contenido del boton
 const divDado = document.getElementById("divDado")
 const resultado = document.getElementById("resultado")
 let giro
@@ -229,13 +251,17 @@ divDado.addEventListener("click", (e) => {
     }else{
         let dado = e.target.textContent
         giroDado(dado)
-        console.log(e.target)
         Swal.fire({
             title: `Tu tirada es:`,
             text: `${giro}`,
             width: 300,
             padding: '1em',
             color: '#716add',
-          })
+        })
     }
 })
+
+/* errores a solucionar:
+1) Los obj se sobreescriben en el inventario si vuelvo a mostrar el form.
+2) no funciona el inventario/form de los personajes con indice > 0
+*/
